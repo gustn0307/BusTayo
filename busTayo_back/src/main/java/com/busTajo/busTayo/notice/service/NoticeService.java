f14cd74af53a37d527e32ec95c9a9c40433041d8
yzihun0123 <zihoon0123@gmail.com>
2026-06-19 09:29:44 +0900

    feature: first commit (공지 기능 구현)

---
+++ busTayo_back/src/main/java/com/busTajo/busTayo/notice/service/NoticeService.java
@@ -0,0 +1,51 @@
+package com.busTajo.busTayo.notice.service;
+
+import com.busTajo.busTayo.admin.entity.Notice;
+import com.busTajo.busTayo.notice.dto.NoticeDto;
+import com.busTajo.busTayo.notice.repository.NoticeRepository;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class NoticeService {
+    private final NoticeRepository noticeRepository;
+
+    public List<NoticeDto> findAllNotice() {
+        return noticeRepository
+                .findAll()
+                .stream()
+                .map(x->NoticeDto.toNoticeDto(x))
+                .toList();
+    }
+
+    public NoticeDto findNoticeById(Long id) {
+        Notice notice = noticeRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("공지 없음"));
+        return NoticeDto.toNoticeDto(notice);
+    }
+
+    public void insertNotice(NoticeDto dto) {
+        Notice notice = new Notice();
+        notice.setTitle(dto.getNoticeTitle());
+        notice.setContent(dto.getNoticeContent());
+        noticeRepository.save(notice);
+    }
+
+    @Transactional
+    public void updateNotice(NoticeDto noticeDto) {
+        Notice notice = noticeRepository.findById(noticeDto.getNoticeId())
+                .orElseThrow(() -> new RuntimeException("공지 없음"));
+        notice.setTitle(noticeDto.getNoticeTitle());
+        notice.setContent(noticeDto.getNoticeContent());
+    }
+
+    public void deleteNotice(Long id) {
+        Notice notice = noticeRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("공지 없음"));
+        noticeRepository.delete(notice);
+    }
+}
